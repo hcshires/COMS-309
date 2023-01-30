0d75ae83006b758f333fd8cfbb50f8dda673b390
Henry Shires <hcsgeek@gmail.com>
2023-01-30 14:26:48 -0600

    Completed hello world tutorial for Spring Boot + Maven

---
+++ Experiments/henry/helloworld/src/main/java/coms309/WelcomeController.java
@@ -0,0 +1,34 @@
+package coms309;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello and welcome to COMS 309: " + name;
+    }
+
+    @GetMapping("/{name}/{num}")
+    public String goodbye(@PathVariable String name, @PathVariable String num) {
+        try {
+            int val = Integer.parseInt(num);
+
+            if (val == 5) {
+                return "Goodbye: " + val;
+            } else {
+                return "Hello and welcome to COMS 309: " + name;
+            }
+        } catch (NumberFormatException e) {
+            return "Invalid number.";
+        }
+    }
+}
