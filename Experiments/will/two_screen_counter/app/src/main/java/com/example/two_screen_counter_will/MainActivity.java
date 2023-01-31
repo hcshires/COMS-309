88c287f5c92656b5ebc050128e307ef1a90fb69f
Will Maahs <willmaahs@Wills-MacBook-Pro-5.local>
2023-01-31 13:03:41 -0600

    starting two screen counter

---
+++ Experiments/will/two_screen_counter/app/src/main/java/com/example/two_screen_counter_will/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.two_screen_counter_will;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
