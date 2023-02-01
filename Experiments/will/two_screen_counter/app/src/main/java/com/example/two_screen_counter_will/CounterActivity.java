4c707ff6af7ef6635961f8b394f537e174214644
Will Maahs <willmaahs@Wills-MacBook-Pro-5.local>
2023-02-01 13:20:13 -0600

    Working on adding display of counter to homescreen

--- Experiments/will/two_screen_counter/app/src/main/java/com/example/two_screen_counter_will/CounterActivity.java
+++ Experiments/will/two_screen_counter/app/src/main/java/com/example/two_screen_counter_will/CounterActivity.java
@@ -1,51 +1,49 @@
 package com.example.two_screen_counter_will;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.two_screen_counter_will.databinding.ActivityCounterBinding;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityCounterBinding binding;
+    Button btnInc;
+    Button backBtn;
+    TextView numberTxt;
+
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
 
-        binding = ActivityCounterBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        btnInc = findViewById(R.id.btnInc);
+        backBtn = findViewById(R.id.backBtn);
+        numberTxt = findViewById(R.id.number);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_counter);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        btnInc.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v)
+            {
+                numberTxt.setText(String.valueOf(++counter));
             }
         });
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_counter);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(CounterActivity.this, MainActivity.class);
+                i.putExtra("count", counter );
+                startActivity(i);
+            }
+        });
+
+
     }
 }
--- Experiments/will/two_screen_counter/app/src/main/java/com/example/two_screen_counter_will/MainActivity.java
+++ Experiments/will/two_screen_counter/app/src/main/java/com/example/two_screen_counter_will/MainActivity.java
@@ -6,22 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
+    EditText out;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Bundle extra = getIntent().getExtras();
+        if ( extra != null) {
+            Integer counter = extra.getInt("count");
+        }
+
         b1 = findViewById(R.id.buttonToCounter);
+        out = findViewById(R.id.countOut);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Counter)
+                Intent intent = new Intent(MainActivity.this, CounterActivity.class);
             }
         });
+
+
+
     }
+
 }
