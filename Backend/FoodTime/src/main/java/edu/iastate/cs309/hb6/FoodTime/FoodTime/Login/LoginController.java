93c88f7ce2a50a681b2abe5306359597e55e6a47
Anthony Manschula <tonyman2@iastate.edu>
2023-02-09 09:44:41 -0600

    #14: Add base for login class and login REST controller

---
+++ Backend/FoodTime/src/main/java/edu/iastate/cs309/hb6/FoodTime/FoodTime/Login/LoginController.java
@@ -0,0 +1,7 @@
+package edu.iastate.cs309.hb6.FoodTime.FoodTime.Login;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+}
---
+++ Backend/FoodTime/src/main/java/edu/iastate/cs309/hb6/FoodTime/FoodTime/Login/LoginItem.java
@@ -0,0 +1,7 @@
+package edu.iastate.cs309.hb6.FoodTime.FoodTime.Login;
+
+import javax.persistence.Entity;
+
+@Entity
+public class LoginItem {
+}
