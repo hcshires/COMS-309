095a2e7284ce019e748b6bb6ddecbc774b57d00d
Anthony Manschula <tonyman2@iastate.edu>
2023-02-09 09:25:58 -0600

    Configure Spring project for start of backend work

---
+++ Backend/FoodTime/src/main/java/edu/iastate/cs309/hb6/FoodTime/FoodTime/FoodTimeApplication.java
@@ -0,0 +1,13 @@
+package edu.iastate.cs309.hb6.FoodTime.FoodTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTimeApplication.class, args);
+	}
+
+}
