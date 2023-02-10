c811d1fc02f4af8016a6f6e03d06e456d148f43e
Henry Shires <hcsgeek@gmail.com>
2023-02-10 16:02:39 -0600

    Implement Java functionality from tutorial

--- Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/LoginActivity.java
+++ Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/LoginActivity.java
@@ -3,12 +3,56 @@ package edu.iastate.cs309.hb6.foodtime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button loginBtn;
+    EditText email, pwd;
+
+    /**
+     * Create the LoginActivity and manage its widgets
+     * @param savedInstanceState -
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        email = findViewById(R.id.email);
+        pwd = findViewById(R.id.password);
+
+        loginBtn.setOnClickListener(view -> {
+            if (!email.getText().toString().equals("") && !pwd.getText().toString().equals("")) {
+                if (!loginUser().equals("")) {
+                    Intent intent2 = new Intent(LoginActivity.this, DashboardActivity.class);
+                    startActivity(intent2);
+                } else {
+                    Toast.makeText(this, "Email and/or password is incorrect. Please try again.", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Email and/or password field is blank.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     *
+     * @return the UID of an existing user that matches the entered credentials
+     */
+    private String loginUser() {
+        return "";
+    }
+
+    /**
+     *
+     * @return a newly created UID from the backend that matches the entered credentials of this new user
+     */
+    private String createUser() {
+        return "";
     }
 }
--- Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/MainActivity.java
+++ Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/MainActivity.java
@@ -2,13 +2,24 @@ package edu.iastate.cs309.hb6.foodtime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.toLoginBtn);
+        button.setOnClickListener(v -> {
+            Intent intent1 = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent1);
+        });
     }
 }
