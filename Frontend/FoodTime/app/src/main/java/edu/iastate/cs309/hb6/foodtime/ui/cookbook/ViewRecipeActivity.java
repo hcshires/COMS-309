494b9527c4d6eda836c176e8effb7c906d645d9c
Henry Shires <hcsgeek@gmail.com>
2023-04-19 00:08:26 -0500

    Fixed CardView not displaying recipes from database

--- Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/ui/cookbook/ViewRecipeActivity.java
+++ Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/ui/cookbook/ViewRecipeActivity.java
@@ -4,30 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TableLayout;
-import android.widget.TextView;
+import com.google.android.material.tabs.TabLayout;
 
 import edu.iastate.cs309.hb6.foodtime.R;
 
 public class ViewRecipeActivity extends AppCompatActivity {
-
-    private TableLayout ingDircTab;
-    private ViewPager viewRicipeVP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_recipe);
 
-        /*Widgets*/
+        /* Widgets */
         //recipeTitle = (TextView) findViewById(R.id.recipeTitle);
         //ingredientsLV = (ListView) findViewById(R.id.ingredientsListViewer);
-        ingDircTab = (TableLayout) findViewById(R.id.viewRecipeTabLayout);
-        viewRicipeVP = (ViewPager) findViewById(R.id.viewRecipeViewPager);
+
+        TabLayout ingDircTab = findViewById(R.id.viewRecipeTabLayout);
+        ViewPager viewRicipeVP = findViewById(R.id.viewRecipeViewPager);
+
         //ingDircTab;
         //recipeTitle.setText(set text from put extras);
-
     }
 }
