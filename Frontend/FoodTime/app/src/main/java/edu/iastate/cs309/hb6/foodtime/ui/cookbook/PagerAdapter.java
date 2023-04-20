5d7065f625918847a8e0ee0f6458db53d416b7e6
willmaahs <wmaahs@iastate.edu>
2023-04-19 22:48:02 -0500

    Making tab layout for directions and ingredients

--- Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/ui/cookbook/PagerAdapter.java
+++ Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/ui/cookbook/PagerAdapter.java
@@ -1,46 +1,41 @@
 package edu.iastate.cs309.hb6.foodtime.ui.cookbook;
 
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
 
 import androidx.annotation.NonNull;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
 
-import edu.iastate.cs309.hb6.foodtime.R;
+public class PagerAdapter extends FragmentStateAdapter {
 
+    private final ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+    private final ArrayList<String> fragmentTitles = new ArrayList<>();
 
-public class PagerAdapter extends androidx.viewpager.widget.PagerAdapter {
+    public PagerAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
 
-    public PagerAdapter() {
-
+    /**
+     * @param position
+     * @return
+     */
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentArrayList.get(position);
     }
 
     /**
      * @return
      */
     @Override
-    public int getCount() {
-        return 0;
+    public int getItemCount() {
+        return fragmentArrayList.size();
     }
 
-    /**
-     * @param view   Page View to check for association with <code>object</code>
-     * @param object Object to check for association with <code>view</code>
-     * @return
-     */
-    @Override
-    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+    public void addFragment(Fragment fragment, String title) {
+        fragmentArrayList.add(fragment);
+        fragmentTitles.add(title);
     }
-
-
 }
