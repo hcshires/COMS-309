33c7a106c3060496ac7b4232493e2051e5c5cf7a
willmaahs <wmaahs@iastate.edu>
2023-04-27 22:47:32 -0500

    Working on screen to be able to input directions

---
+++ Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/ui/cookbook/AddDirectionActivity.java
@@ -0,0 +1,16 @@
+package edu.iastate.cs309.hb6.foodtime.ui.cookbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.iastate.cs309.hb6.foodtime.R;
+
+public class AddDirectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_direction);
+    }
+}
---
+++ Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/ui/cookbook/IngredientsAdapter.java
@@ -0,0 +1,4 @@
+package edu.iastate.cs309.hb6.foodtime.ui.cookbook;
+
+public class IngredientsAdapter {
+}
