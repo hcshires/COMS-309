ddf46ba083c46327d2c090de71c6a5b2417955cf
Henry Shires <hcsgeek@gmail.com>
2023-05-01 22:31:31 -0500

    Change test UID to match nuked server

--- Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/utils/Const.java
+++ Frontend/FoodTime/app/src/main/java/edu/iastate/cs309/hb6/foodtime/utils/Const.java
@@ -4,62 +4,75 @@ package edu.iastate.cs309.hb6.foodtime.utils;
  * Contains String and other data constants used in API requests, data transfer, and navigation
  */
 public class Const {
-    /** Server URL **/
-    private static final String URL_SERVER = "http://coms-309-011.class.las.iastate.edu:8080";
-    // URL_SERVER = "http://localhost:8080"; // for local testing
 
-    /** API endpoints **/
+    /**
+     * Parent User Testing credentials, username[0], password[1], uid[2]
+     */
+    public static final String[] CRED_PARENT_USER = {"SystemTestUser@COMS309.com", "root", "bd66484d-ae81-492a-9457-9be427aa9998"};
+
     /* Meals */
-
-    /** Add a new meal */
+    /**
+     * Server URL
+     **/
+    private static final String URL_SERVER = "http://coms-309-011.class.las.iastate.edu:8080";
+                             // URL_SERVER = "http://localhost:8080"; // for local testing
+    /**
+     * Add a new meal
+     */
     public static final String URL_MEALS_ADD = URL_SERVER + "/meals/add";
-
-    /** Remove a meal by ID */
-    public static final String URL_MEALS_REMOVE = URL_SERVER + "/meals/remove";
     // NOT USED // public static final String URL_MEALS_UPDATE = URL_SERVER + "/meals/update";
-
-    /** Get meals by day */
-    public static final String URL_MEALS_GET_BYDAY = URL_SERVER + "/meals/get/by-day";
+    /**
+     * Remove a meal by ID
+     */
+    public static final String URL_MEALS_REMOVE = URL_SERVER + "/meals/remove";
     // NOT USED // public static final String URL_MEALS_GET_ALL = URL_SERVER + "/meals/get/all";
 
     /* Pantry */
     // NOT USED // public static final String URL_PANTRY_GETPANTRY = URL_SERVER + "/pantry/getUserPantry";
-
-    /** Get the pantry items as a String */
+    /**
+     * Get meals by day
+     */
+    public static final String URL_MEALS_GET_BYDAY = URL_SERVER + "/meals/get/by-day";
+    /**
+     * Get the pantry items as a String
+     */
     public static final String URL_PANTRY_GETPANTRYITEMS = URL_SERVER + "/pantry/getUserPantryString";
-
-    /** Add an item to the pantry */
+    /**
+     * Add an item to the pantry
+     */
     public static final String URL_PANTRY_ADDITEM = URL_SERVER + "/pantry/addToPantry";
 
-    /** Remove an item from the pantry */
-    public static final String URL_PANTRY_REMOVEITEM = URL_SERVER + "/pantry/removeFromPantry";
-
     /* Recipes */
-    /** Get a recipe by ID */
+    /**
+     * Remove an item from the pantry
+     */
+    public static final String URL_PANTRY_REMOVEITEM = URL_SERVER + "/pantry/removeFromPantry";
+    /**
+     * Get a recipe by ID
+     */
     public static final String URL_RECIPES_GETRECIPE = URL_SERVER + "/recipes/get";
-
-    /** Get all recipes (labels only) */
+    /**
+     * Get all recipes (labels only)
+     */
     public static final String URL_RECIPES_GETLABELS = URL_SERVER + "/recipes/get/labels";
-
-    /** Add a new recipe */
-    public static final String URL_RECIPES_ADDRECIPE = URL_SERVER + "/recipes/add";
     // NOT USED // public static final String URL_RECIPES_REMOVERECIPE = URL_SERVER + "/recipes/remove";
 
     /* User Auth */
-    /** Create a new user */
+    /**
+     * Add a new recipe
+     */
+    public static final String URL_RECIPES_ADDRECIPE = URL_SERVER + "/recipes/add";
+    /**
+     * Create a new user
+     */
     public static final String URL_CREATE_USER = URL_SERVER + "/users/create";
-
-    /** Login a user */
-    public static final String URL_LOGIN_USER = URL_SERVER + "/users/login";
     // NOT USED // public static final String URL_DELETE_USER = URL_SERVER + "/users/delete";
     // NOT USED // static final String URL_RESET_PWD = URL_SERVER + "/users/reset-password";
 
-    /** Other Constants **/
-    /** Child User Testing credentials, username[0], password[1], uid[2] */
-    // NOT USED // public static final String[] CRED_CHILD_USER = {"SystemTestChild@COMS309.com", "root", "b22a34c4-6460-4458-bd43-6c14547259a8"};
-
-    /** Child User Testing credentials, username[0], password[1], uid[2] */
-    public static final String[] CRED_PARENT_USER = {"SystemTestUser@COMS309.com", "root", "6bc95713-067d-4f72-ab2b-0180f759daad"};
+    /**
+     * Login a user
+     */
+    public static final String URL_LOGIN_USER = URL_SERVER + "/users/login";
 
 
 }
