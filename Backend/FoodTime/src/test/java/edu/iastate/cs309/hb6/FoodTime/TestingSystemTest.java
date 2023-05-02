a6bf8d0c7914afe3faafc221aeb568c2ed6c06df
blake hardy <bshardy@iastate.edu>
2023-05-01 21:39:12 -0500

    added pantry tests, made existing tests less bad

--- Backend/FoodTime/src/test/java/edu/iastate/cs309/hb6/FoodTime/TestingSystemTest.java
+++ Backend/FoodTime/src/test/java/edu/iastate/cs309/hb6/FoodTime/TestingSystemTest.java
@@ -11,12 +11,13 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -30,6 +31,14 @@ public class TestingSystemTest {
         RestAssured.port = port;
         RestAssured.baseURI = "http://localhost";
     }
+
+    //TODO so I can remember where this is
+    //makes testing easier and more consistent
+    String testIngredientName = "testFood1234";
+    int testQuantity = 5;
+    String testUnitType = "count";
+    String UID = "bd66484d-ae81-492a-9457-9be427aa9998";
+
 
     @Test
     public void loginUserTest() throws JSONException {
@@ -86,7 +95,7 @@ public class TestingSystemTest {
         Response response = RestAssured.given().
                 header("Content-Type", "application/json").
                 header("charset","utf-8").
-                param("UID", "6bc95713-067d-4f72-ab2b-0180f759daad").
+                param("UID", UID).
                 param("day", "monday").
                 body(mealBody.toString()).
                 when().
@@ -101,7 +110,7 @@ public class TestingSystemTest {
         Response response = RestAssured.given().
                 header("Content-Type", "application/json").
                 header("charset","utf-8").
-                param("UID", "6bc95713-067d-4f72-ab2b-0180f759daad").
+                param("UID", UID).
                 param("day", "monday").
                 when().
                 get("/meals/get/by-day");
@@ -118,7 +127,7 @@ public class TestingSystemTest {
         Response response = RestAssured.given().
                 header("Content-Type", "application/json").
                 header("charset","utf-8").
-                param("UID", "6bc95713-067d-4f72-ab2b-0180f759daad").
+                param("UID", UID).
                 when().
                 get("/meals/get/all");
 
@@ -131,7 +140,7 @@ public class TestingSystemTest {
         Response response = RestAssured.given().
                 header("Content-Type", "application/json").
                 header("charset","utf-8").
-                param("UID", "6bc95713-067d-4f72-ab2b-0180f759daad").
+                param("UID", UID).
                 param("day", "monday").
                 param("mealName", "Quesadillas").
                 param("removeAll", "false").
@@ -140,5 +149,139 @@ public class TestingSystemTest {
 
         int statusCode = response.getStatusCode();
         assertEquals(200, statusCode);
+    }
+
+    //pantry and ingredient tests
+    //Im going to assume these run in order
+
+    @Test
+    public void addToPantryTest(){
+        //add an item to a given user's pantry
+        //PutMapping(path = "/pantry/addToPantry"
+        //Param String UID, Param String ingredientName, Param int quantity, Param String unitsType
+
+        Response response = RestAssured.given().
+                header("Content-Type", "application/json").
+                header("charset","utf-8").
+                param("UID", UID).
+                param("ingredientName", testIngredientName).
+                param("quantity", testQuantity).
+                param("unitsType", testUnitType).
+                when().
+                put("/pantry/addToPantry");
+
+        System.out.println(response.getBody().asString());
+
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    public void getPantryTest(){
+        //return a user's whole pantry
+        //GetMapping(path = "/pantry/getUserPantry")
+        //@RequestParam String UID
+
+        Response response = RestAssured.given().
+                header("Content-Type", "application/json").
+                header("charset","utf-8").
+                param("UID", UID).
+                when().
+                get("/pantry/getUserPantry");
+
+        assertTrue(response.getBody().asString().contains(testIngredientName)); //check if ingredient you just added is in the list
+        assertEquals(200, response.getStatusCode()); //check if successful
+    }
+
+    @Test
+    public void changeQuantityTest(){
+
+        //PutMapping(path = "/pantry/setQuantity"
+        //RequestParam String UID, @RequestParam String ingredientName, @RequestParam int quantity
+
+        //change ingredient quantity value
+        Response response = RestAssured.given().
+                header("Content-Type", "application/json").
+                header("charset","utf-8").
+                param("UID", UID).
+                param("ingredientName", testIngredientName).
+                param("quantity", 6). //!= original value
+                        when().
+                put("/pantry/setQuantity");
+
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    public void changeUnitTest(){
+
+        //PutMapping(path = "/pantry/setQuantityType
+        //Param String UID, @RequestParam String ingredientName, @RequestParam String quantityType
+
+        //change unit type
+        Response response = RestAssured.given().
+                header("Content-Type", "application/json").
+                header("charset","utf-8").
+                param("UID", UID).
+                param("ingredientName", testIngredientName).
+                param("quantityType", "uncountable").
+                when().
+                put("/pantry/setQuantityType");
+
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    public void changeQuantTest(){
+
+        //GetMapping(path = "/pantry/getQuantity"
+        //Param String UID, @RequestParam String ingredientName
+
+        //check for new value
+        Response response = RestAssured.given().
+                header("Content-Type", "application/json").
+                header("charset","utf-8").
+                param("UID", UID).
+                param("ingredientName", testIngredientName).
+                when().
+                get("/pantry/getQuantity");
+
+        assertEquals(200, response.getStatusCode());
+        assertEquals(true, response.getBody().asString().contains("6"));
+    }
+
+    @Test
+    public void changeUnitTypeTest(){
+
+        //GetMapping(path = "/pantry/getQuantityType"
+        //Param String UID, @RequestParam String ingredientName
+
+        //check for new unit type
+        Response response = RestAssured.given().
+                header("Content-Type", "application/json").
+                header("charset","utf-8").
+                param("UID", UID).
+                param("ingredientName", testIngredientName).
+                when().
+                get("/pantry/getQuantityType");
+
+        assertEquals(200, response.getStatusCode());
+        assertEquals(true, response.getBody().asString().contains("uncountable"));
+    }
+
+    @Test
+    public void removeFromPantryTest(){
+        //remove items by name from user's pantry
+        //Delete /pantry/removeFromPantry
+        //Param String UID, Param String ingredientName
+
+        Response response = RestAssured.given().
+                header("Content-Type", "application/json").
+                header("charset","utf-8").
+                param("UID", UID).
+                param("ingredientName", testIngredientName).
+                when().
+                delete("/pantry/removeFromPantry");
+
+        assertEquals(200, response.getStatusCode()); //note that the ingredient does actually need to be present to be removed
     }
 }
